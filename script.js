a1aef610e8eae0cf88f08c0f68f09d7943a7c15b
George Zisis <georgezisis@gmail.com>
2025-05-22 12:07:16 +0300

    Create button functionality and add transition effect

--- script.js
+++ script.js
@@ -17,17 +17,42 @@ eventButton.addEventListener("click", () => {
 
 imgs = Array.from(document.querySelectorAll(".carousel img"))
 
+rightButton = document.querySelector(".rl-button.right")
+leftButton = document.querySelector(".rl-button.left")
+
 let currentIndex = 0
+const maxIndex = Math.floor(imgs.length / 2)
+const minIndex = -maxIndex
 
-const resizeObserver = new ResizeObserver(entries => {
-    for (let entry of entries) {
-        const width = entry.contentRect.width;
+function getImageAt(i) {
+    const centerIndex = Math.floor(imgs.length / 2) // or whatever you consider the center index
+    const total = imgs.length;
+    return images[(centerIndex + i + total) % total]; // wrap safely
+}
 
-        for (img of imgs) {
-            img.style.transform = "translateX(" + (-currentIndex * width) + "px)"
+rightButton.addEventListener("click", () => {
+    currentIndex++
+    if (currentIndex > maxIndex) currentIndex = minIndex
+    console.log(currentIndex)
+    updateCarousel(currentIndex)
+})
+
+leftButton.addEventListener("click", () => {
+    currentIndex--
+    if (currentIndex < minIndex) currentIndex = maxIndex;
+    console.log(currentIndex)
+    updateCarousel(currentIndex)
+})
+
+function updateCarousel(currentIndex) {
+    const resizeObserver = new ResizeObserver(entries => {
+        for (let entry of entries) {
+            const width = entry.contentRect.width;
+
+            for (img of imgs) {
+                img.style.transform = "translateX(" + (-currentIndex * width) + "px)"
+            }
         }
-    }
-});
-
-resizeObserver.observe(imgs[0])
-
+    });
+    resizeObserver.observe(imgs[0])
+}
