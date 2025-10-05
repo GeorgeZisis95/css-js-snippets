7211b0ba52acf84fb548cfea3cc63d36f62797c8
George Zisis <georgezisis@gmail.com>
2025-10-05 09:32:35 +0300

    Fix homepage

---
+++ local-storage/script.js
@@ -0,0 +1,22 @@
+let nodes = []
+
+function openNoteDialog() {
+    const dialog = document.getElementById("noteDialog")
+    const titleInput = document.getElementById("noteTitle")
+    const contentInput = document.getElementById("noteContent")
+
+    dialog.showModal()
+    titleInput.focus()
+}
+
+function closeNoteDialog() {
+    document.getElementById("noteDialog").close()
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("noteDialog").addEventListener("click", (event) => {
+        if (event.target === this) {
+            closeNoteDialog()
+        }
+    })
+})
